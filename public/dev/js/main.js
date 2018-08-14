6239032141dce12846c9fb49ea72e5afff1704d2
Kaluchii <mr.kaluchii@mail.ru>
2018-08-14 23:18:16 +0600

    Fix layouts price

--- public/dev/js/main.js
+++ public/dev/js/main.js
@@ -166,7 +166,12 @@ $(document).ready(function () {
         $('.js_give_area').text(thisLayout.area.replace('.', ','));
         $('.js_give_floor').text(thisLayout.floor);
 
-        var half_tg = number_format(Math.round(thisLayout.meter_cost * thisLayout.area * EXCHANGE_COST), 0, ',', ' ');
+        var half_tg;
+        if(thisLayout.area == '58.9' || thisLayout.area == '59.9' || thisLayout.area == '64.8' || thisLayout.area == '94.3'){
+            half_tg = number_format(Math.round(250000 * thisLayout.area), 0, ',', ' ');
+        } else {
+            half_tg = number_format(Math.round(thisLayout.meter_cost * thisLayout.area * EXCHANGE_COST), 0, ',', ' ');
+        }
         $('.js_give_price').text(half_tg);
         $('.js_area_btn').removeClass('is-active').closest('.layout-choice__btns-item').removeClass('is-active');
         $(this).addClass('is-active').closest('.layout-choice__btns-item').addClass('is-active');
